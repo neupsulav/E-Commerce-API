b6dc2feee0aa0277be0b0792ebebc8ec58b4e9f3
Sulav Neupane <neupsulav@gmail.com>
2023-07-08 10:42:41 +0545

    Added product and category schema

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-
-const dbconnect = require("./connection/connect");
-
+const dbconnect = require("./db/connect");
 const dotenv = require("dotenv");
 dotenv.config();
+const errorHandlerMiddleware = require("./middlewares/ErrorHandlerMiddleware");
+const noRoute = require("./middlewares/noRoute");
+const morgan = require("morgan");
 
+// middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// errorHandlerMiddleware
+app.use(errorHandlerMiddleware);
+
+// noRoute
+app.use(noRoute);
+
+// listen to port
 const listen = async () => {
   try {
     await dbconnect(process.env.MONGO_URI);

7976b64f41cd1cce69a25f45f03a73cc99e1d826
Sulav Neupane <neupsulav@gmail.com>
2023-07-09 12:22:44 +0545

    Added token authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,26 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("./errorHandler");
+const catchAsync = require("./catchAsync");
+
+const authentication = catchAsync(async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new ErrorHandler("Aunthentication failed"), 400);
+  }
+
+  const token = authorization.split(" ")[1];
+
+  const payload = jwt.verify(token, process.env.SECRET_KEY);
+
+  req.user = {
+    userId: payload.userId,
+    userName: payload.userName,
+    userEmail: payload.userEmail,
+  };
+
+  next();
+});
+
+module.exports = authentication;
